f50b17a6bb961a3f0d5ff213b522c3716a89e8ea
hkutangyu <super_tangyu@foxmail.com>
2017-06-09 23:47:15 +0800

    add logo categories and user logo relation table

--- label_app/admin.py
+++ label_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from label_app.models import LogoCategory
+from label_app.models import UserLogoRelation
 
 # Register your models here.
+admin.site.register(LogoCategory)
+admin.site.register(UserLogoRelation)
+
+
--- label_app/models.py
+++ label_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class LogoCategory(models.Model):
+    logo_category = models.CharField(max_length=50, null=False)
+    create_date = models.DateField()
+
+    def __str__(self):
+        return self.logo_category
+
+
+class UserLogoRelation(models.Model):
+    user = models.ForeignKey(User)
+    logo_category = models.ManyToManyField(LogoCategory)
+    create_date = models.DateField()
+
+
+
