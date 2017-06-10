72d5d58509208e78b0885cae3cfbdc4ab9a6ab77
hkutangyu <super_tangyu@foxmail.com>
2017-06-10 15:56:11 +0800

    release mode

--- django_label_site/settings.py
+++ django_label_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&@t#&ji75o*9u$&8i97ztctbseq4v0)qn$r8gp$&p088)&k-=i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
